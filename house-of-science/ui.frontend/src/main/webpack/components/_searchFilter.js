82ef0efecc057975e9e29bade363ceb1b8fea463
Davide Mariotti <103132461+davide-mariotti@users.noreply.github.com>
2023-02-08 15:34:50 +0100

    rollback to j query  slideshow.js

--- house-of-science/ui.frontend/src/main/webpack/components/_searchFilter.js
+++ house-of-science/ui.frontend/src/main/webpack/components/_searchFilter.js
@@ -1,102 +1,104 @@
-// This script fetches a JSON file from the given URL and then stores the data in local storage
-// The data is later retrieved from local storage and filtered based on selected criteria
-// The filtered data is then displayed on the page
-
 /* eslint-disable max-len */
 console.log("search Filter OK");
 
+window.onload = function () {
+  // Select the carousel
+  const results = document.getElementById("results");
+  if (results) { 
+    //start
+    
 fetch('https://davide-mariotti.github.io/lodash/data.json')
-    .then(response => response.json()) // Convert response to JSON
-    .then(data => {
-        
-        localStorage.setItem('myData', JSON.stringify(data)); // Store data in local storage as string
-        
-    })
-    .catch(error => console.error(error)); // Log error if fetch fails
+.then(response => response.json()) // Convert response to JSON
+.then(data => {
+    
+    localStorage.setItem('myData', JSON.stringify(data)); // Store data in local storage as string
+    
+})
+.catch(error => console.error(error)); // Log error if fetch fails
 
 
 const data = JSON.parse(localStorage.getItem('myData')); // Retrieve data from local storage
 
 // Define object to hold filter criteria
 const filters = {
-  topic: [],
-  author: [],
-  source: [],
-  year: [],
-  typology: [],
-  Tag: []
+topic: [],
+author: [],
+source: [],
+year: [],
+typology: [],
+Tag: []
 };
 
 // Function to filter data based on criteria
 const filterData = () => _.filter(data, item => {
-  // Check if each filter is empty or if the current item matches the criteria
-  return (
-    (_.isEmpty(filters.topic) || _.includes(filters.topic, item.topic)) &&
-    (_.isEmpty(filters.author) || _.includes(filters.author, item.author)) &&
-    (_.isEmpty(filters.source) || _.includes(filters.source, item.source)) &&
-    (_.isEmpty(filters.year) || _.includes(filters.year, item.year)) &&
-    (_.isEmpty(filters.typology) || _.includes(filters.typology, item.typology)) &&
-    (_.isEmpty(filters.tag) || _.includes(filters.tag, item.tag))
-  );
+// Check if each filter is empty or if the current item matches the criteria
+return (
+(_.isEmpty(filters.topic) || _.includes(filters.topic, item.topic)) &&
+(_.isEmpty(filters.author) || _.includes(filters.author, item.author)) &&
+(_.isEmpty(filters.source) || _.includes(filters.source, item.source)) &&
+(_.isEmpty(filters.year) || _.includes(filters.year, item.year)) &&
+(_.isEmpty(filters.typology) || _.includes(filters.typology, item.typology)) &&
+(_.isEmpty(filters.tag) || _.includes(filters.tag, item.tag))
+);
 });
 
 // Function to display filtered data
 const displayData = (data) => {
-  let html = "";
-  // Loop through filtered data
-  data.forEach(item => {
-    // Build HTML for each item
-    html += `
-          <div class="card">
-            <div class="card-header" id="heading-${item.typology}">
-                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-${item.typology}-${item.date}" aria-expanded="true" aria-controls="collapse-${item.typology}-${item.date}">
-                  <span class="typology">${item.typology}</span>
-                  <span class="description">${item.description}</span>
-                </button>
-                <span class="date">${item.date}</span>
+let html = "";
+// Loop through filtered data
+data.forEach(item => {
+// Build HTML for each item
+html += `
+      <div class="card">
+        <div class="card-header" id="heading-${item.typology}">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-${item.typology}-${item.date}" aria-expanded="true" aria-controls="collapse-${item.typology}-${item.date}">
+              <span class="typology">${item.typology}</span>
+              <span class="description">${item.description}</span>
+            </button>
+            <span class="date">${item.date}</span>
+        </div>
+        <div id="collapse-${item.typology}-${item.date}" class="collapse" aria-labelledby="heading-${item.typology}-${item.date}" data-parent="#results">
+          <div class="card-body">
+            <div class="meta-data">
+              <div class="meta-data-item Topic">TOPIC: ${item.topic}</div>
+              <div class="meta-data-item Author">AUTHORS: ${item.author}</div>
+              <div class="meta-data-item Source">SOURCE: ${item.source}</div>
+              <div class="meta-data-item Tag">${item.tag}</div>
             </div>
-            <div id="collapse-${item.typology}-${item.date}" class="collapse" aria-labelledby="heading-${item.typology}-${item.date}" data-parent="#results">
-              <div class="card-body">
-                <div class="meta-data">
-                  <div class="meta-data-item Topic">TOPIC: ${item.topic}</div>
-                  <div class="meta-data-item Author">AUTHORS: ${item.author}</div>
-                  <div class="meta-data-item Source">SOURCE: ${item.source}</div>
-                  <div class="meta-data-item Tag">${item.tag}</div>
-                </div>
-                <div class="description">
-                ABSTRACT:<br>
-                  ${item.description}
-                </div>
-                <div class="containerLink">
-                  <a class="Link" href="${item.link}" target="_blank">Read more</a>
-                </div>
-              </div>
+            <div class="description">
+            ABSTRACT:<br>
+              ${item.description}
+            </div>
+            <div class="containerLink">
+              <a class="Link" href="${item.link}" target="_blank">Read more</a>
             </div>
           </div>
-        `;
-  });
-  document.getElementById("results").innerHTML = html;
+        </div>
+      </div>
+    `;
+});
+document.getElementById("results").innerHTML = html;
 };
 
 
 const search = () => {
-  // Removes 'filter-selected' class from all elements with class 'filter-selected'
-  document.querySelectorAll('.filter-selected').forEach(el => el.classList.remove('filter-selected'));
-  // Adds 'filter-selected' class to the parent of all checked checkboxes
-  [...document.querySelectorAll("input[type='checkbox']:checked")].forEach(el => el.parentNode.classList.add('filter-selected'));
+// Removes 'filter-selected' class from all elements with class 'filter-selected'
+document.querySelectorAll('.filter-selected').forEach(el => el.classList.remove('filter-selected'));
+// Adds 'filter-selected' class to the parent of all checked checkboxes
+[...document.querySelectorAll("input[type='checkbox']:checked")].forEach(el => el.parentNode.classList.add('filter-selected'));
 
-  // Filters the data based on selected topics, authors, sources, years, typologies, and tags
-  filters.topic = [...document.querySelectorAll("#topic-filter input[type='checkbox']:checked")].map(x => x.value);
-  filters.author = [...document.querySelectorAll("#author-filter input[type='checkbox']:checked")].map(x => x.value);
-  filters.source = [...document.querySelectorAll("#source-filter input[type='checkbox']:checked")].map(x => x.value);
-  filters.year = [...document.querySelectorAll("#year-filter input[type='checkbox']:checked")].map(x => x.value);
-  filters.typology = [...document.querySelectorAll("#typology-filter input[type='checkbox']:checked")].map(x => x.value);
-  filters.tag = [...document.querySelectorAll("#tag-filter input[type='checkbox']:checked")].map(x => x.value);
-  
-  const filteredData = filterData(); // Get the filtered data from the filterData function 
-  displayData(filteredData); // Displays the filtered data using the displayData function
-  changeTypologyColor(); // Calls the changeTypologyColor function
-  checkFilterEmpty();
+// Filters the data based on selected topics, authors, sources, years, typologies, and tags
+filters.topic = [...document.querySelectorAll("#topic-filter input[type='checkbox']:checked")].map(x => x.value);
+filters.author = [...document.querySelectorAll("#author-filter input[type='checkbox']:checked")].map(x => x.value);
+filters.source = [...document.querySelectorAll("#source-filter input[type='checkbox']:checked")].map(x => x.value);
+filters.year = [...document.querySelectorAll("#year-filter input[type='checkbox']:checked")].map(x => x.value);
+filters.typology = [...document.querySelectorAll("#typology-filter input[type='checkbox']:checked")].map(x => x.value);
+filters.tag = [...document.querySelectorAll("#tag-filter input[type='checkbox']:checked")].map(x => x.value);
+
+const filteredData = filterData(); // Get the filtered data from the filterData function 
+displayData(filteredData); // Displays the filtered data using the displayData function
+changeTypologyColor(); // Calls the changeTypologyColor function
+checkFilterEmpty();
 };
 
 // Extract unique topics, authors, sources, years, typologies, and tags
@@ -104,42 +106,42 @@ const search = () => {
 const uniqueTopics = _.uniqBy(data, 'topic');
 let options = '';
 uniqueTopics.forEach(topic => {
-  options += `<label><input type="checkbox" value="${topic.topic}" onchange="search()">${topic.topic}</label>`;
+options += `<label><input type="checkbox" value="${topic.topic}" onchange="search()">${topic.topic}</label>`;
 });
 document.getElementById("topic-filter").innerHTML = options;
 
 const uniqueAuthors = _.uniqBy(data, 'author');
 options = '';
 uniqueAuthors.forEach(author => {
-  options += `<label><input type="checkbox" value="${author.author}" onchange="search()">${author.author}</label>`;
+options += `<label><input type="checkbox" value="${author.author}" onchange="search()">${author.author}</label>`;
 });
 document.getElementById("author-filter").innerHTML = options;
 
 const uniqueSources = _.uniqBy(data, 'source');
 options = '';
 uniqueSources.forEach(source => {
-  options += `<label><input type="checkbox" value="${source.source}" onchange="search()">${source.source}</label>`;
+options += `<label><input type="checkbox" value="${source.source}" onchange="search()">${source.source}</label>`;
 });
 document.getElementById("source-filter").innerHTML = options;
 
 const uniqueYears = _.uniqBy(data, 'year');
 options = '';
 uniqueYears.forEach(year => {
-  options += `<label><input type="checkbox" value="${year.year}" onchange="search()">${year.year}</label>`;
+options += `<label><input type="checkbox" value="${year.year}" onchange="search()">${year.year}</label>`;
 });
 document.getElementById("year-filter").innerHTML = options;
 
 const uniqueTypologys = _.uniqBy(data, 'typology');
 options = '';
 uniqueTypologys.forEach(typology => {
-  options += `<label><input type="checkbox" value="${typology.typology}" onchange="search()">${typology.typology}</label>`;
+options += `<label><input type="checkbox" value="${typology.typology}" onchange="search()">${typology.typology}</label>`;
 });
 document.getElementById("typology-filter").innerHTML = options;
 
 const uniqueTags = _.uniqBy(data, 'tag');
 options = '';
 uniqueTags.forEach(tag => {
-  options += `<label><input type="checkbox" value="${tag.tag}" onchange="search()">${tag.tag}</label>`;
+options += `<label><input type="checkbox" value="${tag.tag}" onchange="search()">${tag.tag}</label>`;
 });
 document.getElementById("tag-filter").innerHTML = options;
 
@@ -148,36 +150,36 @@ displayData(data); // Display the data
 
 // Change the color of the typology text based on its value
 function changeTypologyColor() {
-  var elements = document.getElementsByClassName("typology");
-  for (var i = 0; i < elements.length; i++) {
-    // Check the value of each typology text and apply the corresponding color
-    if (elements[i].textContent === "Articles") {
-      elements[i].style.backgroundColor = "red";
-    } if (elements[i].textContent === "Report") {
-      elements[i].style.backgroundColor = "green";
-    } if (elements[i].textContent === "Perspective") {
-      elements[i].style.backgroundColor = "yellow";
-      elements[i].style.color = "black";
-    } if (elements[i].textContent === "Correspondence") {
-      elements[i].style.backgroundColor = "gray";
-    } if (elements[i].textContent === "News") {
-      elements[i].style.backgroundColor = "blue";
-    } if (elements[i].textContent === "Original Investigation") {
-      elements[i].style.backgroundColor = "black";
-    } if (elements[i].textContent === "New Result") {
-      elements[i].style.backgroundColor = "brown";
-    } if (elements[i].textContent === "Editorial") {
-      elements[i].style.backgroundColor = "orange";
-    } if (elements[i].textContent === "Comment") {
-      elements[i].style.backgroundColor = "purple";
-    } if (elements[i].textContent === "Viewpoint") {
-      elements[i].style.backgroundColor = "aqua";
-    } if (elements[i].textContent === "Case Report") {
-      elements[i].style.backgroundColor = "burlywood";
-    } else if (elements[i].textContent === "Letter") {
-      elements[i].style.backgroundColor = "cadetblue";
-    }
-  }
+var elements = document.getElementsByClassName("typology");
+for (var i = 0; i < elements.length; i++) {
+// Check the value of each typology text and apply the corresponding color
+if (elements[i].textContent === "Articles") {
+  elements[i].style.backgroundColor = "red";
+} if (elements[i].textContent === "Report") {
+  elements[i].style.backgroundColor = "green";
+} if (elements[i].textContent === "Perspective") {
+  elements[i].style.backgroundColor = "yellow";
+  elements[i].style.color = "black";
+} if (elements[i].textContent === "Correspondence") {
+  elements[i].style.backgroundColor = "gray";
+} if (elements[i].textContent === "News") {
+  elements[i].style.backgroundColor = "blue";
+} if (elements[i].textContent === "Original Investigation") {
+  elements[i].style.backgroundColor = "black";
+} if (elements[i].textContent === "New Result") {
+  elements[i].style.backgroundColor = "brown";
+} if (elements[i].textContent === "Editorial") {
+  elements[i].style.backgroundColor = "orange";
+} if (elements[i].textContent === "Comment") {
+  elements[i].style.backgroundColor = "purple";
+} if (elements[i].textContent === "Viewpoint") {
+  elements[i].style.backgroundColor = "aqua";
+} if (elements[i].textContent === "Case Report") {
+  elements[i].style.backgroundColor = "burlywood";
+} else if (elements[i].textContent === "Letter") {
+  elements[i].style.backgroundColor = "cadetblue";
+}
+}
 }
 
 // listens to the "load" event of the window and triggers two functions when the event is fired.
@@ -186,24 +188,30 @@ window.addEventListener("load", checkFilterEmpty);
 
 // used to remove elements from the DOM if they don't meet certain conditions.
 function checkFilterEmpty() {
-  // An array of class selectors for elements that should be checked for content.
-  var classes = ['.card-body .Topic', '.card-body .Author', '.card-body .Source', '.card-body .Year', '.card-body .Tag', '.card-body .description'];
-  for (var i = 0; i < classes.length; i++) {
-    var elements = document.querySelectorAll(classes[i]);
-    for (var j = 0; j < elements.length; j++) {
-      if (elements[j].textContent.trim().length <= 0) {
-        elements[j].remove();
-      }
-    }
-  }
-
-  var links = document.querySelectorAll('.card-body .Link');
-  for (var k = 0; k < links.length; k++) {
-    if (links[k].href.length === 42) { // If the length of the link's href attribute is equal to 42, remove the link from the DOM.
-      links[k].remove();
-      //console.log('link rimosso');
-    } else {
-      //console.log('link ok');
-    }
+// An array of class selectors for elements that should be checked for content.
+var classes = ['.card-body .Topic', '.card-body .Author', '.card-body .Source', '.card-body .Year', '.card-body .Tag', '.card-body .description'];
+for (var i = 0; i < classes.length; i++) {
+var elements = document.querySelectorAll(classes[i]);
+for (var j = 0; j < elements.length; j++) {
+  if (elements[j].textContent.trim().length <= 0) {
+    elements[j].remove();
   }
 }
+}
+
+var links = document.querySelectorAll('.card-body .Link');
+for (var k = 0; k < links.length; k++) {
+if (links[k].href.length === 42) { // If the length of the link's href attribute is equal to 42, remove the link from the DOM.
+  links[k].remove();
+  //console.log('link rimosso');
+} else {
+  //console.log('link ok');
+}
+}
+}
+
+    //end
+   } else {
+    console.log("noCarousel");
+  }
+};
--- house-of-science/ui.frontend/src/main/webpack/components/_slideshow.js
+++ house-of-science/ui.frontend/src/main/webpack/components/_slideshow.js
@@ -1,130 +1,76 @@
-window.onload = function () {
-  // Select the carousel
-  const myCarousel = document.getElementById("slideshowCarousel");
-  if (myCarousel) {
-    //start script
-    // Store all videos, all vids, slides, and progress value elements within the carousel
-    let videos = myCarousel.querySelectorAll("video");
-    let allVids = document.querySelectorAll(
-      "#slideshowCarousel .carousel-item"
-    );
-    let slides = myCarousel.querySelectorAll(".carousel-item");
-    let durations = [];
-    let progressBars = document.querySelectorAll(
-      "#slideshowCarousel .slidebutton .progress-value"
-    );
+/* eslint-disable max-len */
+import jQuery from "jquery";
 
-    // Loop over all videos in the carousel
-    allVids.forEach((el, index) => {
-      // Get the video title and subtitle
-      let title = el.querySelector("input.title").value;
-      let subtitle = el.querySelector("input.subtitle").value;
-      let slideButton = document.querySelectorAll(
-        "#slideshowCarousel .slidebutton"
-      );
+// Wrap the code in a self-invoking anonymous function to avoid polluting the global scope
+jQuery(function ($) {
 
-      // If the slide button doesn't have title and subtitle, append them
-      if (
-        slideButton[index].querySelector(".title") == null ||
-        slideButton[index].querySelector(".subtitle") == null
-      ) {
-        let titleElem = document.createElement("div");
-        let subtitleElem = document.createElement("div");
-        titleElem.className = "title";
-        titleElem.textContent = title;
-        subtitleElem.className = "subtitle";
-        subtitleElem.textContent = subtitle;
-        slideButton[index].appendChild(titleElem);
-        slideButton[index].appendChild(subtitleElem);
-      }
-    });
+	const myCarousel = document.getElementById('slideshowCarousel');					// Get the carousel element from the DOM
+	var videos = myCarousel.querySelectorAll('video');									// Get all the videos in the carousel
+	var allVids = $("#slideshowCarousel").find('.carousel-item');						// Get all the carousel items
+	var slides = myCarousel.querySelectorAll('.carousel-item');							// Get all the slides in the carousel
+	var durations = [];																	// Array to store the durations of all videos
+	var progressBars = $("#slideshowCarousel").find('.slidebutton .progress-value'); 	// Get all the progress bars
+		// Pause all videos except the first one
+		allVids.each(function (index, el) {
+			if (index !== 0) {
+				$(this).find('video')[0].pause();
+			}
+		});
 
-    // Add event listener to the carousel for when the slide transitions
-    myCarousel.addEventListener("slide.bs.carousel", (event) => {
-      // Get the index of the active slide
-      let activeSlideIndex = document.querySelector(".slidebutton.active")
-        .dataset.bsSlideTo;
-      // Set the active slide interval to the video duration
-      let animationTime = durations[activeSlideIndex] + "s";
-      console.log(
-        "activeSlideIndex",
-        activeSlideIndex,
-        "animationTime",
-        animationTime
-      );
-      let slides = document.querySelectorAll(
-        "#slideshowCarousel .carousel-item"
-      );
-      slides[activeSlideIndex].setAttribute(
-        "data-bs-interval",
-        durations[activeSlideIndex] * 1000
-      );
+		// Calculate the duration of all videos
+		for (let i = 0; i < videos.length; i++) {
+			videos[i].onloadedmetadata = function () {
+				durations.push(videos[i].duration);
 
-      // Get the previous video being played and set the video being transitioned to
-      let pvid = slides[event.from].querySelectorAll("video")[0];
-      let vid = slides[event.to].querySelectorAll("video")[0];
-      // Check if the video is currently playing
-      let isPlaying = vid.currentTime > 0 && vid.readyState > 2;
-      // Play the video
-      vid.play();
-      // If the video is playing, pause the previous video
-      if (isPlaying) {
-        pvid.pause();
-      }
-    });
+				// Update the interval time of each carousel item with the duration of the video
+				slides[i].setAttribute('data-bs-interval', durations[i] * 1000);
 
-    // Iterate over each video in the carousel
-    videos.forEach((el, index) => {
-      // Get video duration
-      let duration = el.duration;
-      // Store duration in array
-      durations[index] = duration;
-      // Create progress bar inner element
-      let innerElem = document.createElement("div");
-      innerElem.className = "progress-inner";
-      // Create progress bar
-      let progBar = document.createElement("div");
-      progBar.className = "progress-bar";
-      // Append the inner element to the progress bar
-      progBar.appendChild(innerElem);
-      // Set progress bar width based on video duration
-      progBar.style.width = duration + "s";
-      // Append progress bar to progress value
-      progressBars[index].appendChild(progBar);
-      // Update slide data-interval
-      slides[index].setAttribute("data-bs-interval", duration * 1000);
-      // Check if the video has audio
-      let audioAvailable = el.querySelector('source[type="audio/mp4"]') != null;
-      // If audio is available add click event handler
-      if (audioAvailable) {
-        el.addEventListener("click", () => {
-          if (el.paused) {
-            el.play();
-          } else {
-            el.pause();
-          }
-        });
-      }
-    });
+				// Update the animation duration of each progress bar with the duration of the video
+				progressBars[i].style.animationDuration = durations[i] + "s";
+			};
+		}
 
-    // Add event handlers to video
-    videos.forEach((el) => {
-      // Update video progress bar when the video playing
-      el.addEventListener("timeupdate", () => {
-        let currentTime = el.currentTime;
-        let vidIndex = [...videos].indexOf(el);
-        progressBars[vidIndex].querySelector(".progress-inner").style.width =
-          (currentTime / durations[vidIndex]) * 100 + "%";
-      });
+		// Add title and subtitle text to the progress bars
+		allVids.each(function (index, el) {
+			let title = $(this).find('input.title').val();
+			let subtitle = $(this).find('input.subtitle').val();
+			let slideButton = $("#slideshowCarousel").find('.slidebutton');
+			// Check if the title and subtitle elements already exist in the progress bar
+			if((slideButton[index].querySelector('.title') == null) || (slideButton[index].querySelector('.subtitle')  == null)){
+				let titleElem = document.createElement('div');
+				let subtitleElem = document.createElement('div');
+				titleElem.className = 'title';
+				titleElem.textContent = title;
+				subtitleElem.className = 'subtitle';
+				subtitleElem.textContent = subtitle;
+				slideButton[index].append(titleElem);
+				slideButton[index].append(subtitleElem);
+			}			
+		});
+		
+		// Add an event listener to the carousel that listens for when the carousel slides to a new item
+		myCarousel.addEventListener('slide.bs.carousel', event => {
+			// Get the index of the active slide
+			let activeSlideIndex = document.querySelector('.slidebutton.active').dataset.bsSlideTo;
+			// Get the animation time for the current active slide
+			let animationTime = durations[activeSlideIndex] + "s";
+			//console.log("activeSlideIndex", activeSlideIndex, "animationTime", animationTime);
+			// Select all carousel items
+			let slides = $(this).find('.carousel-item');
+			// Set the interval time for the carousel items based on the active slide
+			slides.data("bs-interval", durations[activeSlideIndex] * 1000);
+			// Select the previous video in the carousel
+			let pvid = slides[event.from].querySelectorAll('video')[0];
+			// Select the current video in the carousel
+			let vid = slides[event.to].querySelectorAll('video')[0];
+			// Check if the current video is playing
+			let isPlaying = vid.currentTime > 0 && vid.readyState > 2;
+			// Play the current video
+			vid.play();
+			// If the current video is playing, pause the previous video
+			if (isPlaying) {
+				pvid.pause();
+			}
+		});
 
-      // Pause video when the slide transition starts
-      el.addEventListener("transitionstart", () => {
-        el.pause();
-      });
-    });
-
-    //end script
-  } else {
-    console.log("noCarousel");
-  }
-};
+});
