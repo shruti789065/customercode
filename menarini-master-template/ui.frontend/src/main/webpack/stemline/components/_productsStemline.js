3e6105dd4b12c3c6263b29e91903415b590565fe
Davide Mariotti <103132461+davide-mariotti@users.noreply.github.com>
2023-03-10 18:15:19 +0100

    fix json read delay

--- menarini-master-template/ui.frontend/src/main/webpack/stemline/components/_productsStemline.js
+++ menarini-master-template/ui.frontend/src/main/webpack/stemline/components/_productsStemline.js
@@ -41,49 +41,70 @@ function copyDataFromJson() {
 }
 
 function displayDataproductsStemline() {
-  const productsStemlineSelect = document.getElementById("productsStemlineSelect");
-  const productsStemlineResults = document.getElementById("productsStemlineResults");
+  const productsStemlineSelect = document.getElementById(
+    "productsStemlineSelect"
+  );
+  const productsStemlineResults = document.getElementById(
+    "productsStemlineResults"
+  );
   const data = JSON.parse(localStorage.getItem("productsStemline"));
   const countries = new Set(data.map((item) => item.country));
 
-  // create select element
-  const select = document.createElement("select");
-  select.classList.add("selectStemline", "form-select");
-  select.addEventListener("change", () => {
-    const selectedCountry = select.value;
-    if (selectedCountry) {
-      // show only products from selected country
-      productElements.forEach((element) => {
-        if (element.dataset.country === selectedCountry) {
+  // create custom select element
+  const selectContainer = document.createElement("div");
+  selectContainer.classList.add("select-container");
+
+  const selectLabel = document.createElement("div");
+  selectLabel.classList.add("select-label");
+  selectLabel.textContent = "Select a country";
+
+  const selectArrow = document.createElement("div");
+  selectArrow.classList.add("select-arrow");
+
+  const optionsContainer = document.createElement("div");
+  optionsContainer.classList.add("options-container");
+  optionsContainer.style.display = "none";
+
+  countries.forEach((country) => {
+    const option = document.createElement("div");
+    option.classList.add("option");
+    option.classList.add(country);
+    option.textContent = country;
+    option.addEventListener("click", () => {
+      selectLabel.textContent = country;
+      optionsContainer.style.display = "none";
+      if (country) {
+        // show only products from selected country
+        productElements.forEach((element) => {
+          if (element.dataset.country === country) {
+            element.style.display = "block";
+          } else {
+            element.style.display = "none";
+          }
+        });
+      } else {
+        // show all products when no country selected
+        productElements.forEach((element) => {
           element.style.display = "block";
-        } else {
-          element.style.display = "none";
-        }
-      });
-    } else {
-      // show no products when no country selected
-      productElements.forEach((element) => {
-        element.style.display = "none";
-      });
-    }
+        });
+      }
+    });
+    optionsContainer.appendChild(option);
   });
 
-  // add placeholder option
-  const placeholderOption = document.createElement("option");
-  placeholderOption.text = "Select a country";
-  placeholderOption.value = "";
-  placeholderOption.selected = true;
-  placeholderOption.disabled = true;
-  select.appendChild(placeholderOption);
+  selectContainer.appendChild(selectLabel);
+  selectContainer.appendChild(selectArrow);
+  selectContainer.appendChild(optionsContainer);
+  selectContainer.addEventListener("click", () => {
+    optionsContainer.style.display =
+      optionsContainer.style.display === "none" ? "block" : "none";
+  });
 
-  // add options for each country
-  countries.forEach((country) => {
-    const option = document.createElement("option");
-    option.classList.add("country");
-    option.classList.add(country);
-    option.text = country;
-    option.value = country;
-    select.appendChild(option);
+  // add event listener to window object to hide options-container when clicking outside of select-container
+  window.addEventListener("click", (event) => {
+    if (!selectContainer.contains(event.target)) {
+      optionsContainer.style.display = "none";
+    }
   });
 
   // create product elements
@@ -114,32 +135,33 @@ function displayDataproductsStemline() {
 
   // append select and product elements to container
   productsStemlineSelect.innerHTML = "";
-  productsStemlineSelect.appendChild(select);
+  productsStemlineSelect.appendChild(selectContainer);
   productElements.forEach((element) => {
     productsStemlineResults.appendChild(element);
   });
 
-  // hide all products on first load
+  // show all products on first load
   productElements.forEach((element) => {
     element.style.display = "none";
   });
+
 }
 
 const init = () => {
-    copyDataFromJson();
-    const data = JSON.parse(localStorage.getItem("productsStemline"));
-    if (data && data.length > 0) {
-      displayDataproductsStemline();
-    } else {
-      const intervalId = setInterval(() => {
-        const data = JSON.parse(localStorage.getItem("productsStemline"));
-        if (data && data.length > 0) {
-          clearInterval(intervalId);
-          displayDataproductsStemline();
-        }
-      }, 500);
-    }
-  };
+  copyDataFromJson();
+  const data = JSON.parse(localStorage.getItem("productsStemline"));
+  if (data && data.length > 0) {
+    displayDataproductsStemline();
+  } else {
+    const intervalId = setInterval(() => {
+      const data = JSON.parse(localStorage.getItem("productsStemline"));
+      if (data && data.length > 0) {
+        clearInterval(intervalId);
+        displayDataproductsStemline();
+      }
+    }, 500);
+  }
+};
 
 window.onload = function () {
   const productsStemline = document.getElementById("productsStemline");
