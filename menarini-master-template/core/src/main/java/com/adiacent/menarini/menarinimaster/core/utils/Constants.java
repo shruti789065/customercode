367c45a0033a13634e1b47e5d87bf5088cdd6d7e
francesca.mancini <f.mancini@adiacent.com>
2023-03-13 18:00:23 +0100

    PageCreationListener: patch in regularexpression

--- menarini-master-template/core/src/main/java/com/adiacent/menarini/menarinimaster/core/utils/Constants.java
+++ menarini-master-template/core/src/main/java/com/adiacent/menarini/menarinimaster/core/utils/Constants.java
@@ -25,5 +25,5 @@ public class Constants {
     public static final String JSON = "json";
 
     public static String SERVICE_NAME = "menarinimaster";
-    public static String INTERNAL_MENU_TEMPLATE_REGEXP = "/conf/[\\w]+/settings/wcm/templates/(menarini---details-news|menarini---details-page|menarini---details-product|menarini---product-category|menarini---product-list-pharmaceutical|menarini---product-list-healthcare)";
+    public static String INTERNAL_MENU_TEMPLATE_REGEXP = "/conf/[\\w-]+/settings/wcm/templates/(menarini---details-news|menarini---details-page|menarini---details-product|menarini---product-category|menarini---product-list-pharmaceutical|menarini---product-list-healthcare)";
 }
