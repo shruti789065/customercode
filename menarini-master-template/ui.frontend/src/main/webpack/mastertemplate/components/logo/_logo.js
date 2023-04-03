bcf06ab66365deb5b4f94c808a753cce9b9f2b32
Alessandro Messori <a.messori@adiacent.com>
2023-04-03 16:33:58 +0200

    logo-fix version two

--- menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/logo/_logo.js
+++ menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/logo/_logo.js
@@ -22,22 +22,49 @@ import $ from 'jquery';
 				console.log($siteIdentier);
 			}
 
-
 			$menuLogo = document.querySelector('.cmp-image--logo .cmp-image__image');
 			//$menuLogoAuthor = document.querySelector('.aem-AuthorLayer-Edit .cmp-image--logo .cmp-image__image');
 			$header = document.querySelector('header.cmp-experiencefragment--header');
-			$logoPath = `/content/dam/logo/${$siteIdentier}/`;
+			//$logoPath = `/content/dam/logo/${$siteIdentier}/`;
+			$logoPath = document.getElementById('logoFolder').value;
 
-			if (typeof Granite !== 'undefined' && Granite.author && Granite.author.EditorFrame) {
+			/*if (typeof Granite !== 'undefined' && Granite.author && Granite.author.EditorFrame) {
 				// sei in ambiente Author
-				$menuLogo.src = $logoPath + 'logo-pos.svg';
+				$menuLogo.src = $logoPath + '/logo-pos.svg';
 				//console.log('Sono in Edit Mode!');
 			  } else {
 				// non sei in ambiente Author
 				//console.log('Non sono in Edit Mode');
-				$menuLogo.src = $logoPath + 'logo-neg.svg';
+				$menuLogo.src = $logoPath + '/logo-neg.svg';
 			  }
 
+			  function disableImageSrc() {
+                // selezioniamo la prima immagine presente nella pagina
+
+
+                // controlliamo se l'immagine è stata già disabilitata
+                if ($menuLogo.getAttribute('data-disabled') === 'true') {
+                  return; // se sì, usciamo dalla funzione
+                }
+
+                // salviamo il valore dell'attributo "src" in un nuovo attributo personalizzato
+                $menuLogo.setAttribute('data-src', $menuLogo.getAttribute('src'));
+
+                // svuotiamo l'attributo "src" dell'immagine
+                $menuLogo.setAttribute('src', '');
+
+                // aggiungiamo un attributo personalizzato per tenere traccia della disabilitazione
+                $menuLogo.setAttribute('data-disabled', 'true');
+
+                // aggiungiamo un listener per ripristinare il valore dell'attributo "src" al primo scroll della pagina
+                window.addEventListener('scroll', function restoreImageSrc() {
+                  $menuLogo.setAttribute('src', $menuLogo.getAttribute('data-src'));
+                  $menuLogo.removeAttribute('data-src');
+                  $menuLogo.removeAttribute('data-disabled');
+                  window.removeEventListener('scroll', restoreImageSrc);
+                }, { once: true });
+              }
+			*/
 
 			if ($header !== null) {
 				const observer = new MutationObserver(function (mutationsList, observer) {
@@ -46,9 +73,9 @@ import $ from 'jquery';
 						// se la mutazione riguarda la classe "scrolled-page" dell'elemento header
 						if (mutation.type === 'attributes' && mutation.attributeName === 'class' && $header.classList.contains('scrolled-page')) {
 							// cambiamo dinamicamente l'attributo src dell'immagine del logo
-							$menuLogo.src = $logoPath + 'logo-pos.svg';
+							$menuLogo.src = $logoPath + '/logo-pos.svg';
 						} else {
-							$menuLogo.src = $logoPath + 'logo-neg.svg';
+							$menuLogo.src = $logoPath + '/logo-neg.svg';
 						}
 					}
 				});
