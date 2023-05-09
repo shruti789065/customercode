9255019bef1a5dbf33589be22a51d18a30354f7e
Jasleen Kaur <j.kaur@adiacent.com>
2023-05-09 11:14:09 +0200

    card hover fix css

--- house-of-science/ui.frontend/src/main/webpack/components/card-hover/_card-hover.js
+++ house-of-science/ui.frontend/src/main/webpack/components/card-hover/_card-hover.js
@@ -4,6 +4,30 @@
 	"use strict";
 	const cardContainers = document.querySelector('.cardHoverContainer');
 	if (cardContainers !== null) {
+
+		const windowSizeMobile = window.matchMedia("(max-width: 768px)");
+		const windowSizeDesktop = window.matchMedia("(min-width: 1200px)");
+		if(windowSizeMobile.matches){
+			document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.margin="0";
+			document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.maxWidth="100%";
+		}
+		windowSizeMobile.onchange = (e) => {
+			if(e.matches){
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.margin="0";
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.maxWidth="100%";
+			} else {
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.margin="0 8.33%";
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.maxWidth="84%";
+			}
+		};
+		windowSizeDesktop.onchange = (e) => {
+			if(e.matches){
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.margin="0 auto";
+			} else {
+				document.querySelector('.card-hover-container').closest('.cmp-container--standard_width').style.margin="0 8.33%";
+			}
+		};
+
 		const cards = cardContainers.querySelectorAll('.cardHover');
 
 		cards.forEach(card => {
