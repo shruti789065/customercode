beb79029eeab2d4242894bb591afbde01a08356c
Alessandro Messori <a.messori@adiacent.com>
2023-05-15 14:30:25 +0200

    modifiche templateecc

--- menarini-master-template/ui.apps/src/main/content/jcr_root/apps/menarinimaster/components/oneTrust/clientlib/js/oneTrustButton.js
+++ menarini-master-template/ui.apps/src/main/content/jcr_root/apps/menarinimaster/components/oneTrust/clientlib/js/oneTrustButton.js
@@ -1,5 +1,5 @@
-var existCookiePref = document.querySelector('.cmp-layoutcontainer--footer .cmp-list .ot-sdk-show-settings');
+var existCookiePref = document.querySelector('.cmp-experiencefragment--footer .cmp-list .ot-sdk-show-settings');
 if (existCookiePref == undefined || existCookiePref == '' ) {
-	document.querySelector('.cmp-layoutcontainer--footer .cmp-list').insertAdjacentHTML("beforeend",'<li class="cmp-list__item"><a class="cmp-list__item-link ot-sdk-show-settings"><span class="cmp-list__item-title">Preferenze cookie</span></a></li>')
+	document.querySelector('.cmp-experiencefragment--footer .cmp-list').insertAdjacentHTML("beforeend",'<li class="cmp-list__item"><a class="cmp-list__item-link ot-sdk-show-settings"><span class="cmp-list__item-title">Preferenze cookie</span></a></li>')
 }
 
