4ae221b6624ec32f1883eb881d59ee1aff83eae5
Alessandro Messori <a.messori@adiacent.com>
2023-09-12 14:59:11 +0200

    menu mobile fix

--- menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/tabsmenu/_tabs-menu.js
+++ menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/tabsmenu/_tabs-menu.js
@@ -1,4 +1,3 @@
-import $ from "jquery";
 import { _isDesktop, _prependHtml } from "../../site/_util.js";
 
 const MenuTabs = (() => {
@@ -9,62 +8,72 @@ const MenuTabs = (() => {
     GENERIC_PANEL_CLASS: "cmp-tabs__tabpanel",
   };
 
-  let $tabsMenu,
-    $whiteMenu,
-    $shareButton,
-    $mobileTabsActive,
-    $tablistMobile,
-    $shareOpened = false;
+  let tabsMenu,
+    whiteMenu,
+    shareButton,
+    mobileTabsActive,
+    tablistMobile,
+    shareOpened = false;
 
   function init() {
-    $tabsMenu = $(".tabs-menu");
-    if ($tabsMenu.length === 0) return;
+    tabsMenu = document.querySelector(".tabs-menu");
+    if (!tabsMenu) return;
 
-    $whiteMenu = false;
-    $shareButton = $(".cmp-share-desktop .cmp-button");
+    whiteMenu = false;
+    shareButton = document.querySelector(".cmp-share-desktop .cmp-button");
 
-    
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+    const tabElements = document.querySelectorAll(".cmp-tabs__tab");
+    tabElements.forEach((tab) => {
+      tab.addEventListener("click", handleTabClick);
+    });
 
-    $(window).on("scroll", handleScroll);
-    $(window).on("resize", handleResize);
-    $(".cmp-tabs__tab").on("click", handleTabClick);
     window.addEventListener("resize", _isMobileWindowSize);
 
     if (!_isDesktop() || _isMobileWindowSize()) {
       setupMobileMenu();
-	 
     } else {
       setupDesktopMenu();
     }
   }
 
   function setupMobileMenu() {
-    let navbarText = $(".cmp-navbar .buildingblock").html();
-    let navbarMobile = `<div class="cmp-navbar--text-mobile">${navbarText}</div>`;
-    $(".menu-nav .cmp-tabs__tablist").append(navbarMobile);
-    _toggleMenu();
-	$tablistMobile = $(".cmp-tabs__tablist_mobile");
+    const navbarText = document.querySelector(".cmp-navbar .buildingblock")
+      .innerHTML;
+    const navbarMobile = `<div class="cmp-navbar--text-mobile">${navbarText}</div>`;
+    const tablist = document.querySelector(".menu-nav .cmp-tabs__tablist");
+    const panelMenuTogglers = document.querySelectorAll(".panel-menu-toggler");
+    const tabsMenuContainers = document.querySelectorAll(
+      ".tabs-menu__container"
+    );
 
-    $tablistMobile.each((index, mobItem) => {
-      let dataPanelAttr = $(mobItem).data("cmp-panel-title");
-      let panelClose =
+    tablist.insertAdjacentHTML("beforeend", navbarMobile);
+    _toggleMenu();
+    tablistMobile = document.querySelectorAll(".cmp-tabs__tablist_mobile");
+    tabsMenuContainers.forEach((mobItem) => {
+      const dataPanelAttr = mobItem.getAttribute("data-cmp-panel-title");
+      const panelClose =
         '<div class="panel-menu-toggler"><span></span><span></span></div>';
-      let html = `<div class="tabs-mobile-panel__title">${panelClose}<h3>${dataPanelAttr}</h3></div>`;
+      const html = `<div class="tabs-mobile-panel__title">${panelClose}<h3>${dataPanelAttr}</h3></div>`;
       _prependHtml(mobItem, html);
     });
 
-    $(".panel-menu-toggler").on("click", () => {
-      $(".tabs-menu__container").removeClass("cmp-tabs__tabpanel--active");
-    });
+    $('.panel-menu-toggler').on('click', () => {
+		$('.tabs-menu__container').removeClass('cmp-tabs__tabpanel--active');
+	});
 
-    $(".cmp-button--mobile__toggler").on("click", () => {
-      toggleMobileMenu();
-    });
+    const mobileToggler = document.querySelector(
+      ".cmp-button--mobile__toggler"
+    );
+    if (mobileToggler) {
+      mobileToggler.addEventListener("click", toggleMobileMenu);
+    }
   }
 
   function setupDesktopMenu() {
-    if ($shareButton.length > 0) {
-      $shareButton.on("click", (e) => {
+    if (shareButton) {
+      shareButton.addEventListener("click", (e) => {
         e.preventDefault();
         toggleShareMenu();
       });
@@ -73,21 +82,23 @@ const MenuTabs = (() => {
 
   function handleScroll() {
     if (
-      $("header.cmp-experiencefragment--header").length > 0 &&
-      !$(".cmp-button--mobile").hasClass("cmp-button--mobile__toggler_close")
+      document.querySelector("header.cmp-experiencefragment--header") &&
+      !document
+        .querySelector(".cmp-button--mobile")
+        .classList.contains("cmp-button--mobile__toggler_close")
     ) {
-      if ($(window).scrollTop() > 40) {
+      if (window.scrollY > 40) {
         _addWhiteMenu();
       } else {
-        if ($("header .cmp-tabs__tabpanel--active").length < 1) {
-		}
-		_removeWhiteMenu();
+        if (!document.querySelector("header .cmp-tabs__tabpanel--active")) {
+          _removeWhiteMenu();
+        }
       }
     }
   }
 
   function handleResize() {
-    if ($(window).height() < 600) {
+    if (window.innerHeight < 600) {
       _addWhiteMenu();
     } else {
       console.log("Height > 600");
@@ -100,76 +111,103 @@ const MenuTabs = (() => {
 
   function _isMobileWindowSize() {
     const w = document.documentElement.clientWidth;
-    const tablist = $(".cmp-tabs__tablist");
-    tablist.toggleClass("cmp-tabs__tablist_mobile", w < 1200);
-    return w < 1200;
+    const tablist = document.querySelector(".cmp-tabs__tablist");
+    if (w < 1200) {
+      tablist.classList.add("cmp-tabs__tablist_mobile");
+      return true;
+    } else {
+      tablist.classList.remove("cmp-tabs__tablist_mobile");
+      return false;
+    }
   }
 
   function _addWhiteMenu() {
-    if (!$whiteMenu) {
-      $("header.cmp-experiencefragment--header").addClass("scrolled-page");
-      $whiteMenu = true;
+    if (!whiteMenu) {
+      document
+        .querySelector("header.cmp-experiencefragment--header")
+        .classList.add("scrolled-page");
+      whiteMenu = true;
     }
   }
 
   function _removeWhiteMenu() {
-    if ($whiteMenu) {
-      $("header.cmp-experiencefragment--header").removeClass("scrolled-page");
-      $whiteMenu = false;
+    if (whiteMenu) {
+      document
+        .querySelector("header.cmp-experiencefragment--header")
+        .classList.remove("scrolled-page");
+      whiteMenu = false;
     }
   }
 
   function _toggleMenu() {
-    if ($(".cmp-tabs__tablist_toggler").length > 0) {
-      $(".cmp-tabs__tablist_toggler").toggleClass("cmp-tabs__tablist_mobile");
+    const tablistToggler = document.querySelector(".cmp-tabs__tablist_toggler");
+    if (tablistToggler) {
+      tablistToggler.classList.toggle("cmp-tabs__tablist_mobile");
     }
-    if ($(".tabs-menu__container").length > 0) {
-      $(".tabs-menu__container").toggleClass("cmp-container--menu_item-mobile");
-    }
+    const tabsMenuContainers = document.querySelectorAll(
+      ".tabs-menu__container"
+    );
+    tabsMenuContainers.forEach((container) => {
+      container.classList.toggle("cmp-container--menu_item-mobile");
+    });
   }
 
   function toggleMobileMenu() {
-    if ($(".cmp-button--mobile__toggler_close").length < 1) {
-      $(".cmp-button--mobile").addClass("cmp-button--mobile__toggler_close");
-      $("body").addClass("h-overflow");
+    const mobileButton = document.querySelector(".cmp-button--mobile");
+    if (!mobileButton.classList.contains("cmp-button--mobile__toggler_close")) {
+      mobileButton.classList.add("cmp-button--mobile__toggler_close");
+      document.body.classList.add("h-overflow");
       console.log("menu aperto");
     } else {
-      $tablistMobile.removeClass("cmp-tabs__tablist-opened");
-      $(".tabs-menu__container").removeClass("cmp-tabs__tabpanel--active");
-      $(".cmp-button--mobile").removeClass("cmp-button--mobile__toggler_close");
-      $("body").removeClass("h-overflow");
-      $(".cmp-navbar-overlayer").hide();
+      tablistMobile.forEach((item) => {
+        item.classList.remove("cmp-tabs__tablist-opened");
+      });
+      const tabsMenuContainers = document.querySelectorAll(
+        ".tabs-menu__container"
+      );
+      tabsMenuContainers.forEach((container) => {
+        container.classList.remove("cmp-tabs__tabpanel--active");
+      });
+      mobileButton.classList.remove("cmp-button--mobile__toggler_close");
+      document.body.classList.remove("h-overflow");
+      document.querySelector(".cmp-navbar-overlayer").style.display = "none";
       console.log("menu chiuso");
     }
-    $tablistMobile.toggleClass("cmp-tabs__tablist_mobile-active");
-    $mobileTabsActive = $(".cmp-tabs__tablist_mobile-active .cmp-tabs__tab");
+    tablistMobile.forEach((item) => {
+      item.classList.toggle("cmp-tabs__tablist_mobile-active");
+    });
+    mobileTabsActive = document.querySelectorAll(
+      ".cmp-tabs__tablist_mobile-active .cmp-tabs__tab"
+    );
     _addWhiteMenu();
-    $mobileTabsActive.on("click", () => {
-      $(".cmp-container--menu_item-mobile").addClass(
-        "cmp-container--menu_item-mobile--active"
-      );
+    mobileTabsActive.forEach((mobItem) => {
+      mobItem.addEventListener("click", () => {
+        document
+          .querySelector(".cmp-container--menu_item-mobile")
+          .classList.add("cmp-container--menu_item-mobile--active");
+      });
     });
   }
 
   function toggleShareMenu() {
-    document
-      .querySelector(".cmp-share-desktop .cmp-share__container")
-      .classList.toggle("cmp-share__active");
+    const shareContainer = document.querySelector(
+      ".cmp-share-desktop .cmp-share__container"
+    );
+    shareContainer.classList.toggle("cmp-share__active");
+    shareOpened = document.querySelectorAll(".cmp-share__active").length > 0;
 
-    $shareOpened = document.querySelectorAll(".cmp-share__active").length > 0;
-
-    if (!$shareOpened) {
-      $(".cmp-navbar-overlayer").hide();
-      $(".cmp-share__container").hide();
-      $("body").removeClass("h-overflow");
+    if (!shareOpened) {
+      document.querySelector(".cmp-navbar-overlayer").style.display = "none";
+      shareContainer.style.display = "none";
+      document.body.classList.remove("h-overflow");
     } else {
-      $(".cmp-navbar-overlayer").show();
-      $(".cmp-share__container").show();
-      $("body").addClass("h-overflow");
+      document.querySelector(".cmp-navbar-overlayer").style.display = "block";
+      shareContainer.style.display = "block";
+      document.body.classList.add("h-overflow");
     }
 
-    if ($(window).scrollTop() == 0 && $shareOpened) {
-      $whiteMenu = false;
+    if (window.scrollY == 0 && shareOpened) {
+      whiteMenu = false;
       _addWhiteMenu();
     }
   }
@@ -179,6 +217,6 @@ const MenuTabs = (() => {
   };
 })();
 
-$(function () {
+document.addEventListener("DOMContentLoaded", () => {
   MenuTabs.init();
 });
