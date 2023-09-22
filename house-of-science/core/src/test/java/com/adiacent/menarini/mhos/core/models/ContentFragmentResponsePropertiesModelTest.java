a85cfcde260618e84ffeba995395104f3515a56b
francesca.mancini <f.mancini@adiacent.com>
2023-09-22 18:09:45 +0200

    fix: aggiunto unit test

---
+++ house-of-science/core/src/test/java/com/adiacent/menarini/mhos/core/models/ContentFragmentResponsePropertiesModelTest.java
@@ -0,0 +1,33 @@
+package com.adiacent.menarini.mhos.core.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContentFragmentResponsePropertiesModelTest {
+
+
+    private ContentFragmentResponsePropertiesModel model;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        model = new ContentFragmentResponsePropertiesModel();
+        model.setCreate(true);
+        model.setPath("/path-test");
+        model.setTitle("Title");
+        model.setParentLocation("parent_location");
+        model.setStatusMessage("message");
+
+    }
+    @Test
+    void testGetters() {
+        assertNotNull(model.getTitle());
+        assertNotNull(model.getParentLocation());
+        assertNotNull(model.getPath());
+        assertNotNull(model.getStatusMessage());
+    }
+
+}
