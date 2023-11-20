535b3f16f7a791f932a28c4bbf1edbd2b160ae4e
Alessandro Messori <a.messori@adiacent.com>
2023-11-20 11:17:57 +0100

    vulnerability resolution

--- menarini-master-template/core/src/main/java/com/adiacent/menarini/menarinimaster/core/beans/CountryDepartmentBean.java
+++ menarini-master-template/core/src/main/java/com/adiacent/menarini/menarinimaster/core/beans/CountryDepartmentBean.java
@@ -2,19 +2,19 @@ package com.adiacent.menarini.menarinimaster.core.beans;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CountryDepartmentBean {
 
-		private String title;
-		private String name;
-		private List<KeyValueItem> department;
+	private String title;
+	private String name;
+	private List<KeyValueItem> department;
 
-		public CountryDepartmentBean(String title, String name, List<KeyValueItem> department) {
-			this.title = title;
-			this.name = name;
-			this.department = department;
-		}
-
+	public CountryDepartmentBean(String title, String name, List<KeyValueItem> department) {
+		this.title = title;
+		this.name = name;
+		this.department = new ArrayList<>(department);
+	}
 }
