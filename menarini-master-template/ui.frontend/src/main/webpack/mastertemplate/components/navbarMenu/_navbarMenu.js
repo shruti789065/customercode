48e2276b766d284f2926ad84796ad931c7da8d6c
Alessandro Messori <a.messori@adiacent.com>
2024-01-17 12:32:37 +0100

    aggiustamenti css

--- menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/navbarMenu/_navbarMenu.js
+++ menarini-master-template/ui.frontend/src/main/webpack/mastertemplate/components/navbarMenu/_navbarMenu.js
@@ -86,6 +86,8 @@ const NavbarMenu = (() => {
       if (dataIndex !== currentContainerIndex) {
         currentContainerIndex = dataIndex;
 
+        const body = document.body;
+
         if (_isDesktop()) {
           const displayStyle = navbarSection.style.display;
           navbarSection.style.display =
@@ -108,6 +110,10 @@ const NavbarMenu = (() => {
         if (dataLogoValue !== null) {
           setNavbarBackgroundImage(dataLogoValue);
         }
+
+        // Aggiungi gestione dell'overflow del body
+        body.style.overflow =
+          navbarSection.style.display === "none" ? "auto" : "hidden";
       }
     }
   }
