ad13626d948d177499844be05169134c46b78789
Alessandro Messori <a.messori@adiacent.com>
2024-05-27 18:10:28 +0200

    more css

---
+++ laboratorio-guidotti/ui.frontend/src/main/webpack/components/languagenavigation.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+(function () {
+	"use strict";
+  
+	/* const LANGUAGE_NAVIGATION = {
+	  allHeaders: document.querySelectorAll("header")
+	  
+	}; */
+  
+	/**
+	 * Initializes the Language Navigation
+	 *
+	 * @public
+	 */
+	function init() {
+	  
+	}
+  
+
+  
+	document.addEventListener("DOMContentLoaded", function () {
+	  init();
+	});
+  })();
+  
+
+  
