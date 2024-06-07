a130f1544216720e952662b7cca11786cc34153b
Alessandro Messori <a.messori@adiacent.com>
2024-06-07 18:24:45 +0200

    business card quasi ultimata

--- menarini-master-template/ui.apps/src/main/content/jcr_root/apps/menarinimaster/components/master-carousel/clientlibs/editorhook/js/panelcontainer.js
+++ menarini-master-template/ui.apps/src/main/content/jcr_root/apps/menarinimaster/components/master-carousel/clientlibs/editorhook/js/panelcontainer.js
@@ -17,7 +17,7 @@
     "use strict";
 
     channel.on("cq-editor-loaded", function(event) {
-        console.log("ENTRATO");
+        /*console.log("ENTRATO");*/
         if (window.CQ && window.CQ.CoreComponents && window.CQ.CoreComponents.panelcontainer &&
             window.CQ.CoreComponents.panelcontainer.v1 && window.CQ.CoreComponents.panelcontainer.v1.registry) {
             window.CQ.CoreComponents.panelcontainer.v1.registry.register({
