c483ba405fc513fc3e65a669597a296dae562910
TECHNOLAB\davide.mariotti <d.mariotti@adiacent.com>
2024-06-13 13:29:31 +0200

    fix px to rem title + carousel

--- laboratorio-guidotti/ui.frontend/src/main/webpack/components/_carousel.js
+++ laboratorio-guidotti/ui.frontend/src/main/webpack/components/_carousel.js
@@ -1,14 +1,18 @@
-// core version + navigation, pagination modules:
+// Import core Swiper functionality along with necessary modules
 import Swiper from "swiper";
 import { Navigation, Pagination, Thumbs } from "swiper/modules";
-// import Swiper and modules styles
+
+// Import Swiper and its modules' styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/thumbs";
 
+// Check if the body dataset attribute 'wcmMode' is not 'EDIT'
 if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
+  // Iterate over each element with class 'swiper'
   document.querySelectorAll(".swiper").forEach(function (s) {
+    // Get the relevant navigation and pagination elements for the current Swiper instance
     let next = s.parentElement.querySelector(".swiper-button-next"),
       prev = s.parentElement.querySelector(".swiper-button-prev"),
       pag = s.parentElement.querySelector(".swiper-pagination"),
@@ -18,12 +22,13 @@ if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
       desktopSlidesNumber = slideMode == "gallery" ? 1 : slidesNumber,
       $thumbs = null;
 
+    // If the slide mode is 'gallery', initialize the thumbnail preview
     if (slideMode == "gallery") {
       let ts = s.parentElement.parentElement.parentElement.querySelector(
         ".swiper-container .swiper-thumbs-slider"
       );
 
-      //setup preview markup and then initialize
+      // Set up preview thumbnails by creating new Swiper slides
       s.querySelectorAll(".swiper-slide").forEach(function (sl) {
         const newElement = document.createElement("div");
         newElement.classList.add("swiper-slide");
@@ -33,6 +38,7 @@ if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
         ts.querySelector(".swiper-wrapper").appendChild(newElement);
       });
 
+      // Initialize the thumbnail Swiper instance
       $thumbs = new Swiper(ts, {
         spaceBetween: 5,
         slidesPerView: 8,
@@ -49,9 +55,11 @@ if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
       });
     }
 
+    // Determine if pagination and navigation should be active
     let paginationActive = slideMode != "gallery",
       navigationActive = paginationActive;
 
+    // Initialize the main Swiper instance
     new Swiper(s, {
       slidesPerView: 1,
       spaceBetween: 10,
@@ -64,21 +72,21 @@ if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
       navigation: {
         nextEl: next,
         prevEl: prev,
-        enabled: false,
+        enabled: true,
       },
       breakpoints: {
         768: {
           slidesPerView: tabletSlidesNumber,
           spaceBetween: 50,
           navigation: {
-            enabled: false,
+            enabled: true,
           },
         },
         1200: {
           slidesPerView: desktopSlidesNumber,
           spaceBetween: 50,
           navigation: {
-            enabled: false,
+            enabled: true,
           },
         },
         1550: {
@@ -89,7 +97,7 @@ if (document.querySelector("body").dataset.wcmMode != 'EDIT') {
           },
         },
       },
-      // configure Swiper to use modules
+      // Configure Swiper to use imported modules
       modules: [Navigation, Pagination, Thumbs],
       thumbs: {
         swiper: $thumbs,
