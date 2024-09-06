9291ec46fc467347e05754f0b917f4378e0ef860
Cristian Lucchetta <cristian.lucchetta@jakala.com>
2024-09-06 10:51:14 +0200

    feat(JKL-FME-65): add frontend data validation

--- fondazione/ui.frontend/src/main/webpack/components/signupform/_signupform.js
+++ fondazione/ui.frontend/src/main/webpack/components/signupform/_signupform.js
@@ -9,6 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
   const professionItems = document.querySelectorAll("#professionList li div");
   let selectedProfession = "";
 
+
+  const dropdownButtonCountry = document.querySelector('#dropdownCountryMenuButton');
+  const dropdownMenuCountry = document.querySelector('#countryList');
+  const countryItems = document.querySelectorAll("#countryList li div");
+  let selectedCountry = "";
+
+  let erroeMessagges = [];
+
+  // STYLE FUNCTIONS AND CUSTOM COMPONENT FUNCTIONS
   dropdownButtonMultiple.addEventListener('click', function () {
     dropdownMenuInterests.style.display = dropdownMenuInterests.style.display === 'block' ? 'none' : 'block';
     displayButtonBorderBottom(dropdownButtonMultiple, dropdownMenuInterests);
@@ -23,8 +32,13 @@ document.addEventListener('DOMContentLoaded', function () {
       dropdownMenuProfession.style.display = 'none';
     }
 
+    if (!dropdownButtonCountry.contains(event.target) && !dropdownMenuCountry.contains(event.target)) {
+      dropdownMenuCountry.style.display = 'none';
+    }
+
     displayButtonBorderBottom(dropdownButtonMultiple, dropdownMenuInterests);
     displayButtonBorderBottom(dropdownButtonProfession, dropdownMenuProfession);
+    displayButtonBorderBottom(dropdownButtonCountry, dropdownMenuCountry);
 
   });
 
@@ -58,12 +72,26 @@ document.addEventListener('DOMContentLoaded', function () {
     displayButtonBorderBottom(dropdownButtonProfession, dropdownMenuProfession);
   });
 
+  dropdownButtonCountry.addEventListener('click', function () {
+    dropdownMenuCountry.style.display = dropdownMenuCountry.style.display === 'block' ? 'none' : 'block';
+    displayButtonBorderBottom(dropdownButtonCountry, dropdownMenuCountry);
+  });
+
   professionItems.forEach(element => {
     element.addEventListener('click', function (event) {
       selectedProfession = element.textContent.trim();
       dropdownMenuProfession.style.display = 'none'
       displayButtonBorderBottom(dropdownButtonProfession, dropdownMenuProfession);
       updateDropdownTextProfession();
+    })
+  })
+
+  countryItems.forEach(element => {
+    element.addEventListener('click', function (event) {
+      selectedCountry = element.textContent.trim();
+      dropdownMenuCountry.style.display = 'none'
+      displayButtonBorderBottom(dropdownButtonCountry, dropdownMenuCountry);
+      updateDropdownTextCountry();
     })
   })
 
@@ -87,6 +115,16 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdownButtonProfession.textContent = innerString
   }
 
+  function updateDropdownTextCountry() {
+    let innerString = selectedCountry === "" ? 'Select Country' : selectedCountry;
+    if (innerString !== 'Select Country') {
+      dropdownButtonCountry.classList.add('dropdown-toggle-filled');
+    } else {
+      dropdownButtonCountry.classList.remove('dropdown-toggle-filled');
+    }
+    dropdownButtonCountry.textContent = innerString
+  }
+
   function displayButtonBorderBottom(button, menu) {
     if (menu.style.display === 'block') {
       button.classList.add('border-bottom-0');
@@ -99,37 +137,192 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 
+
+
+  // FORM DATA VALIDATION FUNCTIONS
+  function validateProfession() {
+    let errorElement = document.querySelector('#professionErrorString');
+    let dropdownButton = document.querySelector('#dropdownProfessionMenuButton');
+
+    if (selectedProfession === "") {
+      erroeMessagges.push(
+        {
+          id: "profession",
+          message: "Profession field is mandatory"
+        }
+      )
+      errorElement.innerHTML = "This field is mandatory."
+      dropdownButton.classList.add('cmp-signupform__borderRed');
+      dropdownButton.classList.remove('cmp-signupform__borderGreen');
+    } else {
+      dropdownButton.classList.remove('cmp-signupform__borderRed');
+      dropdownButton.classList.add('cmp-signupform__borderGreen');
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function validateCountry() {
+    let errorElement = document.querySelector('#countryErrorString');
+    let dropdownButton = document.querySelector('#dropdownCountryMenuButton');
+    if (selectedCountry === "") {
+      erroeMessagges.push(
+        {
+          id: "country",
+          message: "Country field is mandatory"
+        }
+      )
+      errorElement.innerHTML = "This field is mandatory."
+      dropdownButton.classList.add('cmp-signupform__borderRed');
+      dropdownButton.classList.remove('cmp-signupform__borderGreen');
+    } else {
+      dropdownButton.classList.remove('cmp-signupform__borderRed');
+      dropdownButton.classList.add('cmp-signupform__borderGreen');
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function validateInterests(data) {
+    let errorElement = document.querySelector('#interestErrorString');
+    let dropdownButton = document.querySelector('#dropdownMultiselectMenuButton');
+
+    if (!data.areasOfInterest || data.areasOfInterest.length === 0) {
+      erroeMessagges.push(
+        {
+          id: "areasOfInterest",
+          message: "Please select at least 1 area of interest"
+        }
+
+      )
+      errorElement.innerHTML = "This field is mandatory."
+      dropdownButton.classList.add('cmp-signupform__borderRed');
+      dropdownButton.classList.remove('cmp-signupform__borderGreen');
+    } else {
+      dropdownButton.classList.remove('cmp-signupform__borderRed');
+      dropdownButton.classList.add('cmp-signupform__borderGreen');
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function validateDataProcessing(data) {
+    let errorElement = document.querySelector('#newsletterErrorString');
+    if (!data.personalDataProcessing) {
+      erroeMessagges.push(
+        {
+          id: "personalDataProcessing",
+          message: "Please select personal data processing preference"
+        }
+      )
+      errorElement.innerHTML = "This field is mandatory."
+    } else {
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function validateNewsLetter(data) {
+    let errorElement = document.querySelector('#dataProcessingErrorString');
+    if (!data.receiveNewsletter) {
+      erroeMessagges.push(
+        {
+          id: "receiveNewsletter",
+          message: "Please select if you want to receive newsletter or not"
+        }
+      )
+      errorElement.innerHTML = "This field is mandatory."
+    } else {
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function validatePassword(data) {
+    if (data.password && data.password !== data.passwordConfirmation) {
+      erroeMessagges.push(
+        {
+          id: "email",
+          message: "Password and password confirmation fields doesn't match"
+        }
+      )
+    }
+  }
+
+  function validateEmail(data) {
+    if (data.email && data.email !== data.emailConfirmation) {
+      erroeMessagges.push(
+        {
+          id: "email",
+          message: "Email and email confirmation fields doesn't match"
+        }
+      )
+    }
+  }
+
+  function validateGdpr(data) {
+    let errorElement = document.querySelector('#privacyErrorString');
+    if (data.privacy === "no" || !data.privacy) {
+      erroeMessagges.push(
+        {
+          id: "privacy",
+          message: "Please accept the privacy information notice before sign up"
+        }
+      )
+      errorElement.innerHTML = "This field is mandatory."
+    } else {
+      errorElement.innerHTML = ""
+    }
+  }
+
+  function generateErrorsAlert() {
+    let errorsAlert = document.querySelector('#cmp-signupform__errorsAlert');
+    let errorsList = document.querySelector('#cmp-signupform__errorsList');
+    errorsList.innerHTML = '';
+
+    if (erroeMessagges.length > 0) {
+      errorsAlert.classList.add("d-block");
+      errorsAlert.classList.remove("d-none");
+
+      erroeMessagges.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item.message;
+        errorsList.appendChild(li);
+      });
+    } else {
+      errorsAlert.classList.remove("d-block");
+      errorsAlert.classList.add("d-none");
+    }
+  }
+
+  // FORM SUBMIT FUNCTIONS
   async function sendData(registrationData) {
     const responseCsrf = await fetch('/libs/granite/csrf/token.json');
     const csrfToken = await responseCsrf.json();
-    console.log('CSRF-Token:',csrfToken);
-    const regResponse = await fetch("/bin/api/awsSignUp",{
-      method:"POST",
+    console.log('CSRF-Token:', csrfToken);
+    const regResponse = await fetch("/bin/api/awsSignUp", {
+      method: "POST",
       headers: {
-        'CSRF-Token':csrfToken.token
+        'CSRF-Token': csrfToken.token
       },
-      body:JSON.stringify(registrationData)
+      body: JSON.stringify(registrationData)
 
     });
     const dataResponse = await regResponse.json();
-    console.log(JSON.stringify(dataResponse,null,3));
+    console.log(JSON.stringify(dataResponse, null, 3));
     return dataResponse;
 
   }
 
-
   let form = document.querySelector('#signUpForm');
 
-  if(form) {
-    form.addEventListener("submit",  async(event) => {
+  if (form) {
+    form.addEventListener("submit", async (event) => {
+      erroeMessagges = []; // reset errors array
       event.preventDefault();
+
       const formData = new FormData(form);
 
       let tmpFormData = {
         profession: selectedProfession,
-        areasOfInterest: selectedItemsMultipleSelect.map(x => x.replaceAll(" ",""))
+        areasOfInterest: selectedItemsMultipleSelect.map(x => x.replaceAll(" ", ""))
       };
-  
+
       for (let [key, value] of formData.entries()) {
         tmpFormData = {
           ...tmpFormData,
@@ -137,32 +330,64 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }
 
-      console.log("FORM DATA: ",tmpFormData);
       let registrationData = {
-        "firstName":tmpFormData.firstName,
-        "lastName":tmpFormData.lastName,
-        "birthDate":tmpFormData.dateOfBirth.replaceAll("-",""),
-        "password":tmpFormData.password,
-        "email":tmpFormData.email,
-        "profession":tmpFormData.profession,
-        "phone":tmpFormData.telNumber,
-        "country":tmpFormData.country,
-        "interests":tmpFormData.areasOfInterest,
-        "rolesNames":[],
-        "gender":tmpFormData.gender,
-        "privacyConsent":tmpFormData.privacy,
-        "profilingConsent":tmpFormData.personalDataProcessing,
-        "newsletterConsent":tmpFormData.receiveNewsletter
+        "firstName": tmpFormData.firstName,
+        "lastName": tmpFormData.lastName,
+        "birthDate": tmpFormData.dateOfBirth.replaceAll("-", ""),
+        "password": tmpFormData.password,
+        "email": tmpFormData.email,
+        "profession": tmpFormData.profession,
+        "phone": tmpFormData.telNumber,
+        "country": tmpFormData.country,
+        "interests": tmpFormData.areasOfInterest,
+        "rolesNames": [],
+        "gender": tmpFormData.gender,
+        "privacyConsent": tmpFormData.privacy,
+        "profilingConsent": tmpFormData.personalDataProcessing,
+        "newsletterConsent": tmpFormData.receiveNewsletter
       }
-      console.log("REGISTRATION DATA: ",registrationData);
-      const responseReg = await sendData(registrationData);
-      if(responseReg.cognitoSignUpErrorResponseDto) {
-          alert(JSON.stringify(responseReg.cognitoSignUpErrorResponseDto.message));
-      } else {
-        document.location.href = "/us/welcome";
+
+      validateProfession(tmpFormData);
+      validateCountry(tmpFormData);
+      validateInterests(tmpFormData);
+      validatePassword(tmpFormData);
+      validateEmail(tmpFormData);
+      validateGdpr(tmpFormData);
+      validateDataProcessing(tmpFormData);
+      validateNewsLetter(tmpFormData);
+
+      generateErrorsAlert();
+
+      if (erroeMessagges.length === 0) {
+
+        // const responseReg = await sendData(registrationData);
+        // if(responseReg.cognitoSignUpErrorResponseDto) {
+        //     alert(JSON.stringify(responseReg.cognitoSignUpErrorResponseDto.message));
+        // } else {
+        //   document.location.href = "/us/welcome";
+        // }
       }
     });
   }
 });
+
+function setFieldAsNotValid() {
+  var forms = document.getElementsByClassName('needs-validation');
+
+  var validation = Array.prototype.filter.call(forms, function (form) {
+    form.addEventListener('submit', function (event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+}
+
+window.addEventListener('load', function () {
+  setFieldAsNotValid();
+}, false);
+
 
 
