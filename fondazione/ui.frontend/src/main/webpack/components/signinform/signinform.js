0a725316ac5727b8fe7f92ea75a275e464e3fa07
Cristian Lucchetta <cristian.lucchetta@jakala.com>
2024-09-10 15:02:29 +0200

    feat(JKL-FME-64): add data validation frontend and error alert

--- fondazione/ui.frontend/src/main/webpack/components/signinform/signinform.js
+++ fondazione/ui.frontend/src/main/webpack/components/signinform/signinform.js
@@ -5,74 +5,84 @@ let alertBtn = document.querySelector('#alertBtn');
 async function sendData(logInData) {
     const responseCsrf = await fetch('/libs/granite/csrf/token.json');
     const csrfToken = await responseCsrf.json();
-    console.log('CSRF-Token:',csrfToken);
-    const signIResponse = await fetch("/bin/api/awsSignIn",{
-        method:"POST",
+    const signIResponse = await fetch("/bin/api/awsSignIn", {
+        method: "POST",
         headers: {
-            'CSRF-Token':csrfToken.token
+            'CSRF-Token': csrfToken.token
         },
-        body:JSON.stringify(logInData)
+        body: JSON.stringify(logInData)
 
     });
     const dataResponse = await signIResponse.json();
-    console.log(JSON.stringify(dataResponse,null,3));
     return dataResponse;
 }
 
 function showAlert(message) {
     let alert = $("#alertBox");
-    if(alert) {
-        console.log("alert message: ", message);
-        $("#alertMessage").html( '<strong>' + message + '</strong>');
+    if (alert) {
+        $("#alertMessage").html('<div>' + message + '</div>');
         alert.show();
     }
 
 }
 
 function updateTokens(signInResponse, rememberCheck) {
-    console.log("remember me ?", rememberCheck);
-    if(rememberCheck) {
-        console.log("local storage");
-        localStorage.setItem("token",signInResponse.AuthenticationResult.IdToken);
-        localStorage.setItem("refresh_token",signInResponse.AuthenticationResult.RefreshToken);
+    if (rememberCheck) {
+        localStorage.setItem("token", signInResponse.AuthenticationResult.IdToken);
+        localStorage.setItem("refresh_token", signInResponse.AuthenticationResult.RefreshToken);
     } else {
-        console.log("session storage");
-        sessionStorage.setItem("token",signInResponse.AuthenticationResult.IdToken);
+        sessionStorage.setItem("token", signInResponse.AuthenticationResult.IdToken);
     }
 }
 
-if(alertBtn) {
-    alertBtn.addEventListener("click",(event) => {
+if (alertBtn) {
+    alertBtn.addEventListener("click", (event) => {
         let alert = $("#alertBox");
-        if(alert) {
+        if (alert) {
             alert.hide();
         }
     })
 }
 
-if(signInForm) {
-    signInForm.addEventListener("submit", async(event) =>{
+if (signInForm) {
 
+    signInForm.addEventListener("submit", async (event) => {
         event.preventDefault();
 
         let emailForm = document.querySelector("#emailField");
         let passForm = document.querySelector("#passField");
         let rememberCheck = document.querySelector("#rememberCheck");
-        console.log("remember: ",rememberCheck.checked);
+
         let singIdData = {
             email: emailForm.value,
-            password:passForm.value,
+            password: passForm.value,
         }
         const responseSig = await sendData(singIdData);
-        console.log("SIGN IN RESPONSE: ",responseSig);
-        if(responseSig.cognitoSignInErrorResponseDto) {
+
+        if (responseSig.cognitoSignInErrorResponseDto) {
             showAlert(responseSig.cognitoSignInErrorResponseDto.message);
         } else {
-            updateTokens(responseSig,rememberCheck.checked);
-            //document.location.href = "/";
+            updateTokens(responseSig, rememberCheck.checked);
+            const redirectUrl = sessionStorage.getItem('redirectUrl');
+            document.location.href = redirectUrl;
         }
     });
 }
 
+function setFieldAsNotValid() {
+    var forms = document.getElementsByClassName('needs-validation');
 
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}
 
+window.addEventListener('load', function () {
+    setFieldAsNotValid();
+}, false);
