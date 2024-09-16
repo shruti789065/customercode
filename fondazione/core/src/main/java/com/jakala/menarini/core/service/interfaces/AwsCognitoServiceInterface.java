c0b1932aa517a9dd2af3f6a0e85bb35f6a4eadcb
Filippo Alfio Cicolò <filippo.cicolo@jakala.com>
2024-09-16 11:06:42 +0200

    feat(JKL-FME-128): add forget password api.

--- fondazione/core/src/main/java/com/jakala/menarini/core/service/interfaces/AwsCognitoServiceInterface.java
+++ fondazione/core/src/main/java/com/jakala/menarini/core/service/interfaces/AwsCognitoServiceInterface.java
@@ -8,5 +8,7 @@ public interface AwsCognitoServiceInterface {
     public SignInResponseDto loginOnCognito(SignInDto signInDto);
     public SignInResponseDto refreshOnCognito(RefreshDto requestRefreshDto);
     public SignUpDtoResponse registerOnCognito(SignUpDto registrationData);
+    public ForgetPasswordResponseDto forgetPassword(ForgetPasswordDto forgetPasswordDto);
+    public ConfirmForgetPasswordResponseDto confirmForgetPassword(ConfirmForgetPasswordDto forgetPassword);
 
 }
