0f213560700e136651fc01d984d3fe016e54aa80
Filippo Alfio Cicolò <filippo.cicolo@jakala.com>
2024-10-15 18:20:00 +0200

    chore(FME-134): remove unused logs

--- fondazione/core/src/main/java/com/jakala/menarini/core/service/RoleService.java
+++ fondazione/core/src/main/java/com/jakala/menarini/core/service/RoleService.java
@@ -1,19 +1,16 @@
 package com.jakala.menarini.core.service;
 
 
-import com.jakala.menarini.core.dto.RegisteredUserRoleDto;
+
 import com.jakala.menarini.core.dto.RoleDto;
-import com.jakala.menarini.core.entities.RegisteredUser;
 import com.jakala.menarini.core.entities.RegisteredUserRole;
 import com.jakala.menarini.core.entities.Role;
 import com.jakala.menarini.core.service.interfaces.RoleServiceInterface;
 import org.jooq.*;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -21,8 +18,6 @@ import java.util.List;
 
 @Component(service = RoleServiceInterface.class)
 public class RoleService implements RoleServiceInterface {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserRegisteredService.class);
 
     @Reference(target = "(datasource.name=fondazione-mysql)")
     private DataSource dataSource;
@@ -32,11 +27,10 @@ public class RoleService implements RoleServiceInterface {
     public List<RoleDto> getRoles() {
         try (Connection connection = dataSource.getConnection()) {
             DSLContext create = DSL.using(connection, SQLDialect.MYSQL);
-            List<RoleDto> roles = create.select()
+            return create.select()
                     .from(RoleDto.table)
                     .fetch().into(RoleDto.class);
-            return roles;
-        } catch (SQLException e) {
+        } catch (SQLException | DataAccessException e) {
             return null;
         }
     }
@@ -53,11 +47,8 @@ public class RoleService implements RoleServiceInterface {
                                     .from(RegisteredUserRole.REGISTERED_USER_ROLE)
                                     .where(RegisteredUserRole.REGISTERED_USER_ROLE.REGISTERED_USER_ID.eq(id))
                     ));
-            List<RoleDto> results = roleQuery.fetch().into(RoleDto.class);
-            return results;
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-            e.printStackTrace();
+            return roleQuery.fetch().into(RoleDto.class);
+        } catch (SQLException | DataAccessException e) {
             return null;
         }
     }
