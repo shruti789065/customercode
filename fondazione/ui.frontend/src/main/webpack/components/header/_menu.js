12652859210eb1fa3b43b59c392865c92f6dbe93
Alexander Dunchev <alexander.dunchev@ffw.com>
2024-10-28 16:42:42 +0200

    FME-191: Remove jQuery from menu.js and clean-up utils.js.
    Also add function doc comments to the utis functions.

--- fondazione/ui.frontend/src/main/webpack/components/header/_menu.js
+++ fondazione/ui.frontend/src/main/webpack/components/header/_menu.js
@@ -1,4 +1,3 @@
-import $ from "jquery";
 import { _isDesktop, _prependHtml } from "../../site/_util.js";
 
 const MenuTabs = (() => {
@@ -11,78 +10,124 @@ const MenuTabs = (() => {
 
   const fragmentMenuClass = ".cmp-experiencefragment--header";
 
-  let $tabsMenu, $whiteMenu, $mobileTabsActive, $tablistMobile, shareOpened;
+  let tabsMenu, whiteMenu, mobileTabsActive, tablistMobile, shareOpened;
 
+  /**
+   * Initializes the menu tabs and sets up event listeners.
+   */
   function init() {
-    $tabsMenu = $(".tabs-menu");
-    if ($tabsMenu.length === 0) {
+    tabsMenu = document.querySelector(".tabs-menu");
+    if (!tabsMenu) {
       return;
     }
 
-    $(".tabs-menu__container").removeClass(CONST.ACTIVE_PANEL);
-    $(".menu-nav li")
-      .removeClass(CONST.ACTIVE_TAB)
-      .attr("aria-selected", "false")
-      .attr("tabindex", "-1");
-
-    $(document).on("click", ".menu-nav li", function (e) {
-      e.preventDefault();
-
-      const tabpanelId = $(this).attr("aria-controls");
-      const tabpanel = $("#" + tabpanelId);
-
-      const isActiveTab = $(this).hasClass(CONST.ACTIVE_TAB);
-
-      if (isActiveTab) {
-        $(this)
-          .removeClass(CONST.ACTIVE_TAB)
-          .attr("aria-selected", "false")
-          .attr("tabindex", "-1");
-
-        $(".tabs-menu__container")
-          .removeClass(CONST.ACTIVE_PANEL)
-          .attr("aria-hidden", "true")
-          .hide();
-      } else {
-        $(this)
-          .addClass(CONST.ACTIVE_TAB)
-          .attr("aria-selected", "true")
-          .attr("tabindex", "0");
-
-        tabpanel
-          .addClass(CONST.ACTIVE_PANEL)
-          .attr("aria-hidden", "false")
-          .show();
-
-        _addWhiteMenu();
-        $whiteMenu = true;
-      }
+    document.querySelectorAll(".tabs-menu__container").forEach(container => {
+      container.classList.remove(CONST.ACTIVE_PANEL);
     });
 
-    $whiteMenu = false;
-    $(window).on("scroll", handleScroll);
-    $(window).on("resize", handleResize);
-
-    window.addEventListener("resize", _isMobileWindowSize);
-
-    $(".cmp-tabs__tab .cmp-link--text").on("click", function (event) {
-      event.stopPropagation();
-      _addWhiteMenu();
+    document.querySelectorAll(".menu-nav li").forEach(tab => {
+      tab.classList.remove(CONST.ACTIVE_TAB);
+      tab.setAttribute("aria-selected", "false");
+      tab.setAttribute("tabindex", "-1");
     });
 
-    if (!_isDesktop() || _isMobileWindowSize()) {
+    document.addEventListener("click", handleTabClick);
+
+    whiteMenu = false;
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    window.addEventListener("resize", isMobileWindowSize);
+
+    document.querySelectorAll(".cmp-tabs__tab .cmp-link--text").forEach(link => {
+      link.addEventListener("click", function (event) {
+        event.stopPropagation();
+        addWhiteMenu();
+      });
+    });
+
+    if (!_isDesktop() || isMobileWindowSize()) {
       setupMobileMenu();
     } else {
       setupDesktopMenu();
     }
   }
 
+  /**
+   * Handles clicks on tabs to activate or deactivate them.
+   * @param {Event} e - The click event.
+   */
+  function handleTabClick(e) {
+    if (!e.target.closest(".menu-nav li")) {
+      return false;
+    }
+
+    e.preventDefault();
+    const tab = e.target.closest(".menu-nav li");
+    const tabpanelId = tab.getAttribute("aria-controls");
+    const tabpanel = document.getElementById(tabpanelId);
+    const isActiveTab = tab.classList.contains(CONST.ACTIVE_TAB);
+
+    if (isActiveTab) {
+      deactivateTab(tab);
+      deactivatePanel();
+    } else {
+      activateTab(tab);
+      activatePanel(tabpanel);
+      addWhiteMenu();
+      whiteMenu = true;
+    }
+  }
+
+  /**
+   * Activates the given tab element.
+   * @param {Element} tab - The tab element to activate.
+   */
+  function activateTab(tab) {
+    tab.classList.add(CONST.ACTIVE_TAB);
+    tab.setAttribute("aria-selected", "true");
+    tab.setAttribute("tabindex", "0");
+  }
+
+  /**
+   * Deactivates the given tab element.
+   * @param {Element} tab - The tab element to deactivate.
+   */
+  function deactivateTab(tab) {
+    tab.classList.remove(CONST.ACTIVE_TAB);
+    tab.setAttribute("aria-selected", "false");
+    tab.setAttribute("tabindex", "-1");
+  }
+
+  /**
+   * Activates the given panel element.
+   * @param {Element} panel - The panel element to activate.
+   */
+  function activatePanel(panel) {
+    panel.classList.add(CONST.ACTIVE_PANEL);
+    panel.setAttribute("aria-hidden", "false");
+    panel.style.display = "block";
+  }
+
+  /**
+   * Deactivates all panel elements.
+   */
+  function deactivatePanel() {
+    document.querySelectorAll(`.${CONST.GENERIC_PANEL_CLASS}`).forEach(panel => {
+      panel.classList.remove(CONST.ACTIVE_PANEL);
+      panel.setAttribute("aria-hidden", "true");
+      panel.style.display = "none";
+    });
+  }
+
+  /**
+   * Sets up the mobile version of the menu.
+   */
   function setupMobileMenu() {
-    const tabsMenuContainers = document.querySelectorAll(
-      ".tabs-menu__container"
-    );
-    _toggleMenu();
-    $tablistMobile = $(".cmp-tabs__tablist_mobile");
+    const tabsMenuContainers = document.querySelectorAll(".tabs-menu__container");
+    toggleMenu();
+    tablistMobile = document.querySelector(".cmp-tabs__tablist_mobile");
+
     tabsMenuContainers.forEach((mobItem) => {
       const dataPanelAttr = mobItem.getAttribute("data-cmp-panel-title");
       const panelClose = '<div class="panel-menu-toggler"></div>';
@@ -90,99 +135,122 @@ const MenuTabs = (() => {
       _prependHtml(mobItem, html);
     });
 
-    $(".panel-menu-toggler").on("click", () => {
-      $(".tabs-menu__container").removeClass(CONST.ACTIVE_PANEL);
+    document.querySelectorAll(".panel-menu-toggler").forEach(toggler => {
+      toggler.addEventListener("click", () => {
+        tabsMenuContainers.forEach(container => {
+          container.classList.remove(CONST.ACTIVE_PANEL);
+        });
+      });
     });
 
-    $(".cmp-button--mobile__toggler").on("click", () => {
+    document.querySelector(".cmp-button--mobile__toggler").addEventListener("click", () => {
       toggleMobileMenu();
     });
   }
 
+  /**
+   * Sets up the desktop version of the menu.
+   */
   function setupDesktopMenu() {
     console.log("desktop");
   }
 
+  /**
+   * Handles the scroll event to adjust the menu appearance.
+   */
   function handleScroll() {
-    const isFragmentMenuPresent = $(`${fragmentMenuClass}`).length > 0;
-    const isMobileMenuOpen = $(".cmp-button--mobile").hasClass(
-      "cmp-button--mobile__toggler_close"
-    );
-    const isScrolledPastThreshold = $(window).scrollTop() > 40;
-    const isActiveTabPanelAbsent =
-      $("header .cmp-tabs__tabpanel--active").length < 1;
+    const isFragmentMenuPresent = document.querySelector(fragmentMenuClass) !== null;
+    const isMobileMenuOpen = document.querySelector(".cmp-button--mobile")
+                             .classList.contains("cmp-button--mobile__toggler_close");
+    const isScrolledPastThreshold = window.scrollY > 40;
+    const isActiveTabPanelAbsent = document.querySelector("header .cmp-tabs__tabpanel--active") === null;
 
     if (isFragmentMenuPresent && !isMobileMenuOpen) {
       if (isScrolledPastThreshold) {
-        _addWhiteMenu();
+        addWhiteMenu();
       } else if (isActiveTabPanelAbsent) {
         if (shareOpened) {
-          _addWhiteMenu();
+          addWhiteMenu();
         } else {
-          _removeWhiteMenu();
+          removeWhiteMenu();
         }
       }
     }
   }
 
+  /**
+   * Handles the resize event to adjust the menu layout for smaller screens.
+   */
   function handleResize() {
-    if ($(window).height() < 600) {
-      _addWhiteMenu();
+    if (window.innerHeight < 600) {
+      addWhiteMenu();
     }
   }
 
-  function _isMobileWindowSize() {
-    const w = document.documentElement.clientWidth;
-    const tablist = $(".cmp-tabs__tablist");
-    tablist.toggleClass("cmp-tabs__tablist_mobile", w <= 1200);
-    return w <= 1200;
+  /**
+   * Checks if the window size indicates a mobile layout.
+   * @returns {boolean} True if the window is in mobile size.
+   */
+  function isMobileWindowSize() {
+    const width = document.documentElement.clientWidth;
+    const tablist = document.querySelector(".cmp-tabs__tablist");
+    tablist.classList.toggle("cmp-tabs__tablist_mobile", width <= 1200);
+    return width <= 1200;
   }
 
-  function _addWhiteMenu() {
-    if (!$whiteMenu) {
-      $(`${fragmentMenuClass}`).addClass("scrolled-page");
-      $whiteMenu = true;
+  /**
+   * Adds a white background to the menu for better visibility.
+   */
+  function addWhiteMenu() {
+    if (!whiteMenu) {
+      document.querySelector(fragmentMenuClass).classList.add("scrolled-page");
+      whiteMenu = true;
     }
   }
 
-  function _removeWhiteMenu() {
-    if ($whiteMenu) {
-      $(`${fragmentMenuClass}`).removeClass("scrolled-page");
-      $whiteMenu = false;
+  /**
+   * Removes the white background from the menu.
+   */
+  function removeWhiteMenu() {
+    if (whiteMenu) {
+      document.querySelector(fragmentMenuClass).classList.remove("scrolled-page");
+      whiteMenu = false;
     }
   }
 
-  function _toggleMenu() {
-    if ($(".cmp-tabs__tablist_toggler").length > 0) {
-      $(".cmp-tabs__tablist_toggler").toggleClass("cmp-tabs__tablist_mobile");
-    }
-    if ($(".tabs-menu__container").length > 0) {
-      $(".tabs-menu__container").toggleClass("cmp-container--menu_item-mobile");
-    }
+  /**
+   * Toggles the menu between mobile and desktop view.
+   */
+  function toggleMenu() {
+    document.querySelector(".cmp-tabs__tablist_toggler")?.classList.toggle("cmp-tabs__tablist_mobile");
+    document.querySelector(".tabs-menu__container")?.classList.toggle("cmp-container--menu_item-mobile");
   }
 
+  /**
+   * Toggles the mobile menu state and adjusts UI accordingly.
+   */
   function toggleMobileMenu() {
-    if ($(".cmp-button--mobile__toggler_close").length < 1) {
-      $(".cmp-button--mobile").addClass("cmp-button--mobile__toggler_close");
-      $("body").addClass("h-overflow");
-      _addWhiteMenu();
+    const mobileToggler = document.querySelector(".cmp-button--mobile__toggler_close");
+    if (!mobileToggler) {
+      document.querySelector(".cmp-button--mobile").classList.add("cmp-button--mobile__toggler_close");
+      document.body.classList.add("h-overflow");
+      addWhiteMenu();
     } else {
-      $tablistMobile.removeClass("cmp-tabs__tablist-opened");
-      $(".tabs-menu__container").removeClass(CONST.ACTIVE_PANEL);
-      $(".cmp-button--mobile").removeClass("cmp-button--mobile__toggler_close");
-      $("body").removeClass("h-overflow");
-      $(".cmp-navbar-overlayer").hide();
-      _removeWhiteMenu();
+      tablistMobile.classList.remove("cmp-tabs__tablist-opened");
+      document.querySelectorAll(".tabs-menu__container").forEach(container => {
+        container.classList.remove(CONST.ACTIVE_PANEL);
+      });
+      document.querySelector(".cmp-button--mobile").classList.remove("cmp-button--mobile__toggler_close");
+      document.body.classList.remove("h-overflow");
+      document.querySelector(".cmp-navbar-overlayer").style.display = "none";
+      removeWhiteMenu();
     }
-    $tablistMobile.toggleClass("cmp-tabs__tablist_mobile-active");
-    $mobileTabsActive = document.querySelectorAll(
-      ".cmp-tabs__tablist_mobile-active .cmp-tabs__tab"
-    );
-    $mobileTabsActive.forEach((mobItem) => {
+    tablistMobile.classList.toggle("cmp-tabs__tablist_mobile-active");
+    mobileTabsActive = document.querySelectorAll(".cmp-tabs__tablist_mobile-active .cmp-tabs__tab");
+    mobileTabsActive.forEach(mobItem => {
       mobItem.addEventListener("click", () => {
-        document
-          .querySelector(".cmp-container--menu_item-mobile")
-          .classList.add("cmp-container--menu_item-mobile--active");
+        document.querySelector(".cmp-container--menu_item-mobile").classList
+        .add("cmp-container--menu_item-mobile--active");
       });
     });
   }
@@ -192,6 +260,6 @@ const MenuTabs = (() => {
   };
 })();
 
-$(function () {
+document.addEventListener("DOMContentLoaded", () => {
   MenuTabs.init();
 });
