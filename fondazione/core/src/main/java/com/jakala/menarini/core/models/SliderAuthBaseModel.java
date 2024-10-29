dafdba373ee3dc8c2ac651d841943f81f5e3b427
Filippo Alfio Cicolò <filippo.cicolo@jakala.com>
2024-10-29 09:22:12 +0100

    feat(FME-102): create the SliderAuthBaseModel and refactor EventListingModel with new EventListingService for get filtered events

---
+++ fondazione/core/src/main/java/com/jakala/menarini/core/models/SliderAuthBaseModel.java
@@ -0,0 +1,89 @@
+package com.jakala.menarini.core.models;
+
+import com.jakala.menarini.core.dto.EventModelDto;
+import com.jakala.menarini.core.dto.EventModelRetrunDto;
+import com.jakala.menarini.core.dto.RegisteredUserDto;
+import com.jakala.menarini.core.dto.RegisteredUserTopicDto;
+import com.jakala.menarini.core.service.interfaces.EventListingServiceInterface;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Model(adaptables = {
+        Resource.class,
+        SlingHttpServletRequest.class
+}
+)
+public class SliderAuthBaseModel extends AuthBaseModel {
+
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+
+    @OSGiService
+    private EventListingServiceInterface eventListingService;
+
+    private List<RegisteredUserTopicDto> topics = new ArrayList<>();
+    private List<EventModelDto> events = new ArrayList<>();
+
+    @Override
+    @PostConstruct
+    protected void init()  {
+        super.init();
+        ArrayList<String> idTopics = new ArrayList<>();
+        Resource res = resourceResolver.adaptTo(Resource.class);
+        String language = ModelHelper.getCurrentPageLanguage(resourceResolver, res);
+        if(this.isAuth()) {
+            RegisteredUserDto user = this.getUser();
+            if(!this.rolesData[0].getName().equals("user")) {
+                this.topics = user.getRegisteredUserTopics();
+                if(!this.topics.isEmpty()) {
+                    for (RegisteredUserTopicDto topic : topics ) {
+                        idTopics.add(topic.getTopic().getId());
+                    }
+
+                }
+            }
+
+        }
+        EventModelRetrunDto returnDto = eventListingService.getEvents(
+                resourceResolver,
+                language,
+                idTopics,
+                null,
+                null,
+                null,
+                "",
+                "",
+                1,
+                3
+        );
+        this.events = returnDto.getEvents();
+
+    }
+
+    public List<RegisteredUserTopicDto> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<RegisteredUserTopicDto> topics) {
+        this.topics = topics;
+    }
+
+    public List<EventModelDto> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<EventModelDto> events) {
+        this.events = events;
+    }
+}
