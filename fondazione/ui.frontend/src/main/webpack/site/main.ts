aced016829b37dfa28e43ef609e9b2351711d4c1
Cristian Lucchetta <cristian.lucchetta@jakala.com>
2024-10-30 15:34:34 +0100

    feat(FME-190): import components file main ts

--- fondazione/ui.frontend/src/main/webpack/site/main.ts
+++ fondazione/ui.frontend/src/main/webpack/site/main.ts
@@ -4,7 +4,11 @@ import "./main.scss";
 // Javascript or Typescript
 import "./**/*.js";
 import "./**/*.ts";
+import "../components/carousel/**/*.js";
+import "../components/courses-events/**/*.js";
 import "../components/header/**/*.js";
+import "../components/languagenavigation/**/*.js";
+import "../components/topics/**/*.js";
 
 
 
