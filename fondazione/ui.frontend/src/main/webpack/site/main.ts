1eb32704acf18ced0197e98744943140b6314d23
Cristian Lucchetta <cristian.lucchetta@jakala.com>
2024-10-30 12:06:42 +0100

    feat(FME-190): fix js components import

--- fondazione/ui.frontend/src/main/webpack/site/main.ts
+++ fondazione/ui.frontend/src/main/webpack/site/main.ts
@@ -4,7 +4,10 @@ import "./main.scss";
 // Javascript or Typescript
 import "./**/*.js";
 import "./**/*.ts";
-// import "../components/**/!{signinform,signupform,editprofileform,editimage}/**/*.js";
-// import "../components/**/*.js";
+import "../components/**/carousel/**/*.js";
+import "../components/**/courses-events/**/*.js";
+import "../components/**/header/**/*.js";
+import "../components/**/languagenavigation/**/*.js";
+import "../components/**/topics/**/*.js";
 
 
