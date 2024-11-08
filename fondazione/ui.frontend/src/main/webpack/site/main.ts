2152eab6d4b3e7e88935b059eed656008dbb5e72
Alexander Dunchev <alexander.dunchev@ffw.com>
2024-11-08 13:40:17 +0200

    Fix FE build for divider component and address PR comments.

--- fondazione/ui.frontend/src/main/webpack/site/main.ts
+++ fondazione/ui.frontend/src/main/webpack/site/main.ts
@@ -4,8 +4,4 @@ import "./main.scss";
 // Javascript or Typescript
 import "./**/*.js";
 import "./**/*.ts";
-import "../components/site/carousel/**/*.js";
-import "../components/site/courses-events/**/*.js";
-import "../components/site/header/**/*.js";
-import "../components/site/languagenavigation/**/*.js";
-import "../components/site/topics/**/*.js";
+import "../components/site/**/*.js";
